b70aa9da1e5873d92429f7f66e76dd125d471ef4
Tony <tonyyu1129@gmail.com>
2024-05-30 17:53:22 -0700

    added some tailwind utilities

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,42 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      animation: {
+        "bring-to-front": "bring-to-front 750ms ease-in-out forwards",
+        "move-to-back": "move-to-back 750ms ease-in-out forwards",
+      },
+      keyframes: {
+        "bring-to-front": {
+          "0%": { transform: "translateX(32px) scale(0.9)", zIndex: "0" },
+          "50%": { transform: "translateX(300px) scale(1)", zIndex: "0" },
+          "70%": { zIndex: "1" },
+          "100%": { transform: "translateX(0)", zIndex: "1" },
+        },
+        "move-to-back": {
+          "0%": { transform: "translateX(0) scale(1)", zIndex: "1" },
+          "50%": { transform: "translateX(0px) scale(0.9)", zIndex: "1" },
+          "70%": { zIndex: "0" },
+          "100%": { transform: "translateX(32px) scale(0.9)", zIndex: "0" },
+        },
+      },
+      colors: {
+        "app-black": "#13141b",
+        "app-white": "#EEE",
+        pink: "#ea7bfe",
+        green: "#5bdd66",
+      },
+      boxShadow: {
+        "pink-glow": "0px 0px 10px var(--pink)",
+      },
+      dropShadow: {
+        "pink-glow": "0px 0px 5px var(--pink)",
+      },
+      fontSize: {
+        "1rem": "1rem",
+        "2rem": "2rem",
+        "3rem": "3rem",
+        "4rem": "4rem",
+        "5rem": "5rem",
       },
     },
   },
