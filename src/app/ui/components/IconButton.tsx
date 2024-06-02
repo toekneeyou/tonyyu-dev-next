cca32b5d69a6638982c5fd0b681960d94953aff2
Tony <tonyyu1129@gmail.com>
2024-06-02 11:02:22 -0700

    add disabled styling to IconButton

--- src/app/ui/components/IconButton.tsx
+++ src/app/ui/components/IconButton.tsx
@@ -1,23 +1,33 @@
-import { HTMLAttributes } from "react";
+import { ButtonHTMLAttributes } from "react";
 import { turquoise } from "@/app/lib/constants";
 import { classNames } from "@/app/lib/utils";
 import { IconProp } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  FontAwesomeIcon,
+  FontAwesomeIconProps,
+} from "@fortawesome/react-fontawesome";
 
-interface IconButtonProps extends HTMLAttributes<HTMLButtonElement> {
+interface IconButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   icon: IconProp;
+  size?: FontAwesomeIconProps["size"];
+  className?: string;
 }
 
 export default function IconButton({
   icon,
+  size = "2x",
+  className = "",
+
   ...buttonAttributes
 }: IconButtonProps) {
   return (
     <button
-      className={classNames("appearance-none centered")}
+      className={classNames("appearance-none centered disabled:opacity-25", {
+        [className]: !!className,
+      })}
       {...buttonAttributes}
     >
-      <FontAwesomeIcon icon={icon} color={turquoise} size={"2x"} />
+      <FontAwesomeIcon icon={icon} color={turquoise} size={size} />
     </button>
   );
 }
