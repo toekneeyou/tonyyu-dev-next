91991165ec405108380e4bda0a4514dbc32bec01
Tony <tonyyu1129@gmail.com>
2024-06-02 11:04:18 -0700

    finish projects sectino for mobile viewports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { classNames } from "./lib/utils";
 import AboutMe from "./ui/features/AboutMe";
 import Hero from "./ui/features/Hero";
+import Projects from "./ui/features/Projects";
 import Skills from "./ui/features/Skills";
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <Hero />
       <AboutMe />
       <Skills />
+      <Projects />
     </main>
   );
 }
