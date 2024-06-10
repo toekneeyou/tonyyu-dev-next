e8d1103c14d3cdcf164eb09236868c722f6e61ff
Tony <tonyyu1129@gmail.com>
2024-06-10 16:07:02 -0700

    link header to home page and don't hide in blog

--- src/app/ui/features/Header.tsx
+++ src/app/ui/features/Header.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 import { classNames } from "@/app/lib/utils";
 import { josefin } from "../fonts";
 import IconButton from "../components/IconButton";
@@ -10,6 +13,7 @@ interface HeaderProps {}
 
 export default function Header({}: HeaderProps) {
   const { currentSection } = useSectionContext();
+  const pathname = usePathname();
 
   const handleMenu = () => {
     console.log("menu");
@@ -19,7 +23,10 @@ export default function Header({}: HeaderProps) {
     <header
       className={classNames(
         "z-50 fixed top-0 left-0 w-full p-4 flex justify-between items-center bg-app-black transition-transform duration-300",
-        { "translate-y-[-100%]": currentSection.section === "hero" }
+        {
+          "translate-y-[-100%]":
+            currentSection.section === "hero" && !pathname.match(/blog/),
+        }
       )}
     >
       <h2
@@ -28,7 +35,7 @@ export default function Header({}: HeaderProps) {
           "text-xl font-bold leading-none translate-y-[2px]"
         )}
       >
-        <a href="">TONY YU</a>
+        <Link href="/">TONY YU</Link>
       </h2>
       <div>
         <IconButton icon={faBars} onClick={handleMenu} />
