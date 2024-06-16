cc1654c5e7cf0d4661176a9d32f262c997bb603f
Tony <tonyyu1129@gmail.com>
2024-06-15 20:04:49 -0700

    continued

--- src/app/ui/features/Body.tsx
+++ src/app/ui/features/Body.tsx
@@ -6,25 +6,18 @@ import { inter } from "../fonts";
 import Header from "./Header";
 import Footer from "./Footer";
 import SideMenu from "./SideMenu";
-import { useSideMenuState } from "../contexts/SideMenuContext";
 
 interface BodyProps {
   children: ReactNode;
 }
 
 export default function Body({ children }: BodyProps) {
-  const { isSideMenuOpen } = useSideMenuState();
-
   return (
     <body
       className={classNames(
         `${inter.className} antialiased`,
         "text-app-white bg-app-black",
-        "m-0 h-vh  overflow-x-hidden",
-        {
-          "overflow-y-scroll": !isSideMenuOpen,
-          "overflow-y-hidden": isSideMenuOpen,
-        }
+        "m-0 h-vh  overflow-x-hidden"
       )}
     >
       <Header />
