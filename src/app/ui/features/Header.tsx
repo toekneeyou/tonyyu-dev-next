1b4d3a392cb7ebbf3dfe7ec4cad3d972785370e2
Tony <tonyyu1129@gmail.com>
2024-06-18 18:04:18 -0700

    conditionally hide header on home page

--- src/app/ui/features/Header.tsx
+++ src/app/ui/features/Header.tsx
@@ -2,15 +2,16 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useEffect, useRef } from "react";
+import { faBars, faClose } from "@fortawesome/free-solid-svg-icons";
 
 import { classNames } from "@/app/lib/utils";
 import { josefin } from "../fonts";
 import IconButton from "../components/IconButton";
-import { faBars, faClose } from "@fortawesome/free-solid-svg-icons";
-
 import { useSideMenuAPI, useSideMenuState } from "../contexts/SideMenuContext";
 import { useViewportContext } from "../contexts/ViewportContext";
 import { mdViewport } from "@/app/lib/constants";
+import { HERO } from "@/app/lib/id";
 
 interface HeaderProps {}
 
@@ -19,9 +20,39 @@ export default function Header({}: HeaderProps) {
   const { toggleSideMenu } = useSideMenuAPI();
   const { w } = useViewportContext();
   const pathname = usePathname();
+  const headerRef = useRef<HTMLElement>(null);
+
+  useEffect(() => {
+    const isHome = pathname === "/";
+
+    if (isHome) {
+      const header = headerRef.current!;
+
+      const observer = new IntersectionObserver(
+        (entries) => {
+          entries.forEach((entry) => {
+            if (entry.intersectionRatio < 0.5) {
+              header.classList.remove("translate-y-[-100%]");
+              header.classList.add("translate-y-0");
+            } else {
+              header.classList.remove("translate-y-0");
+              header.classList.add("translate-y-[-100%]");
+            }
+          });
+        },
+        { threshold: [0, 0.25, 0.5, 0.75, 1] }
+      );
+
+      observer.observe(document.getElementById(HERO)!);
+      return () => {
+        observer.disconnect();
+      };
+    }
+  }, [pathname]);
 
   return (
     <header
+      ref={headerRef}
       className={classNames(
         "z-50 fixed top-0 left-0 right-0 h-16 p-4 flex justify-between items-center bg-app-black transition-transform duration-300"
       )}
