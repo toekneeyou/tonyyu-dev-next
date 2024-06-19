138d0385e2d7d41e8fdc7fb34d13d767e72ea783
Tony <tonyyu1129@gmail.com>
2024-06-19 11:37:06 -0700

    load hero image immediately

--- src/app/ui/features/home/heroSection/HeroImage.tsx
+++ src/app/ui/features/home/heroSection/HeroImage.tsx
@@ -5,9 +5,13 @@ import tonyIceland from "../../../../../../public/images/tony-iceland-2160x2699.
 export default function HeroImage() {
   return (
     <div
-      className={classNames("hidden", "md:flex md:sticky md:top-0 md:h-[50%] md:w-full")}
+      className={classNames(
+        "hidden",
+        "md:flex md:sticky md:top-0 md:h-[50%] md:w-full"
+      )}
     >
       <Image
+        priority={true}
         className="object-cover h-full w-full"
         src={tonyIceland}
         alt="Author wearing a redjacket standing in front of a snow-capped mountain in Iceland."
