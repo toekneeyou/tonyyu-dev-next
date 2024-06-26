68db6f4ac602e7257709945a9c7d90d6b5e11f45
Tony <tonyyu1129@gmail.com>
2024-06-25 22:41:16 -0700

    use bodyRef from RefContext

--- src/app/ui/features/Body.tsx
+++ src/app/ui/features/Body.tsx
@@ -4,10 +4,11 @@ import { ReactNode } from "react";
 import dynamic from "next/dynamic";
 import { classNames } from "@/app/lib/utils";
 import { inter } from "../fonts";
-import Header from "./Header";
+import Header from "./header/Header";
 import Footer from "./Footer";
 import { useViewportContext } from "../contexts/ViewportContext";
 import { lgViewport } from "@/app/lib/constants";
+import { useRefContext } from "../contexts/RefContext";
 
 const SideMenu = dynamic(() => import("./SideMenu"));
 
@@ -17,8 +18,11 @@ interface BodyProps {
 
 export default function Body({ children }: BodyProps) {
   const { w } = useViewportContext();
+  const { bodyRef } = useRefContext();
+
   return (
     <body
+      ref={bodyRef}
       className={classNames(
         `${inter.className} antialiased text-app-white bg-app-black m-0 h-vh overflow-x-hidden`
       )}
